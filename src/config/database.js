1787250cb67570b88f9b6bc4ddabd23987009c7b
Douglas Morais <mr.douglasmorais23@gmail.com>
2021-08-24 22:30:29 -0300

    refec form view

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'ziggy.db.elephantsql.com',
+  username: 'nvltissw',
+  password: 'azpLqK6zm7H3bjN6HGD8YpxIar3T_VT3',
+  database: 'nvltissw',
+  define: {
+    timeStamps: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+}
